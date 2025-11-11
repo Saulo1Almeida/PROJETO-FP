14f72075116ba078d1ac50df0d51622229c9abc6
Saulo1Almeida <seas@cesar.school>
2025-11-11 19:53:55 -0300

    menu.py

--- menu.py
+++ menu.py
@@ -1,2 +1,16 @@
 from crud_menu import menu_crud_alunos, menu_crud_professores, menu_crud_turmas
 from relatorios import exibir_menu_relatorios
+
+def exibir_menu():
+   while True:
+        print("\n========== SISTEMA DE GESTÃO ESCOLAR COMUNITÁRIA ==========")
+        print("1. Gerenciar Alunos (CRUD)")
+        print("2. Gerenciar Professores (CRUD)")
+        print("3. Gerenciar Turmas (CRUD)")
+        print("4. Relatórios")
+        print("5. Sair")
+        print("=============================================================")
+
+        opcao = input("Escolha uma opção: ")
+
+      
