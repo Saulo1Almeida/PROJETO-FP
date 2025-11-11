af8a0826632aab9452f3b273b1660d04c9b2f37a
Saulo1Almeida <seas@cesar.school>
2025-11-11 20:10:39 -0300

    turmas.py
    Adicionando função  ler turmas

--- turmas.py
+++ turmas.py
@@ -6,4 +6,16 @@ def buscarIdTurma(turmas, id_turma):
             return turma
     return None
 
+def ler_turmas():
+    turmas = carregaramentodedados("turmas")
+
+    if not turmas:
+        print("Nenhuma turma cadastrada.\n")
+        return
+
+    print("\nLista de Turmas:")
+    for t in turmas:
+        print(f"ID: {t['id']} | Nome: {t['nome']} | Professor ID: {t['professor_id']}")
+    print()
+
 
